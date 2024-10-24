c101efa0ab4bf6cf12ff0461c53421863e3da1ed
Georgios Floros <ge1554fl-s@student.lu.se>
2024-10-24 14:38:49 +0200

    Steps 2 and 3

--- main.cxx
+++ main.cxx
@@ -1,5 +1,7 @@
 
 #include <iostream>
+#include "csv.h"
+#include "date.h"
 int main(int argc, char *argv[]) {
   std::cout << "I am just a code template, you need to implement the "
                "functionality you want to use yourself!"
@@ -8,8 +10,10 @@ int main(int argc, char *argv[]) {
   std::cout << "We were passed " << argc
             << " command line arguments, the first of which was " << argv[0]
             << std::endl;
+
   std::cout << "With a good CLI library, we could use the command line "
                "arguments to make a useful program."
             << std::endl;
+
   return 0;
 }
