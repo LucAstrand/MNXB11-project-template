0bad03ef4bee65e68657f22da84cd96d2965790f
Georgios Floros <ge1554fl-s@student.lu.se>
2024-10-25 11:37:56 +0200

    changes to main & make

--- main.cxx
+++ main.cxx
@@ -8,16 +8,14 @@
 using namespace date;
 using namespace std::chrono;
 
-int main(int argc, char *argv[]) {
-  const std::string csv_file = {"./datasets/test.csv"}
-  io::CSVReader<4> in(csv_file);
-  in.read_header(io::ignore_extra_column, "date", "hour", "temperature", "quality");
-  date::year_month_day date;
-  int year, month, day; 
-  double temperature;
-  char quality;
-  while(in.read_row(date, hour, temperature, quality)){
-    // do stuff with the data
+int main(int agc, char *argv[]r) {
+  io::CSVReader<4, io::trim_chars<>, io::no_quote_escape<','>> in("./datasets/test.csv");
+  in.read_header(io::ignore_extra_column, "day", "year", "month", "measurement");
+  int day, year, month; 
+  double measurement;
+  while (in.read_row(day, year, month, measurement)) {
+        std::cout << "Day: " << day << ", Year: " << year << 
+        ", Month: " << month << ", Measurement: " << measurement << std::endl;
   }
   return 0;
 }
