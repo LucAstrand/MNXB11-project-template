1944298ddddb371748e458f814b74caff9e112ae
Georgios Floros <ge1554fl-s@student.lu.se>
2024-10-30 17:32:47 +0100

    Remove external and build directories

--- build/fmt/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gefloros/git/MNXB11-project-template/fmt
-# Build directory: /home/gefloros/git/MNXB11-project-template/build/fmt
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
