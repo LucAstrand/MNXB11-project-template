5bda7ea75bd20ac4e042a06601253a2f6caead32
Adib Shaker <neoadib@gmail.com>
2024-10-31 23:29:43 +0100

    update comments

--- src/yr_avg_temp.cxx
+++ src/yr_avg_temp.cxx
@@ -73,13 +73,8 @@ void yr_avg_temp(const std::string& startDateStr, const std::string& endDateStr)
             date.tm_min = m->Getmin();
             date.tm_sec = m->Getsec();
             
-            // Store the date and temperature in the map
-            
             if (difftime(mktime(&date), mktime(&startDate)) >= 0 &&
             difftime(mktime(&date), mktime(&endDate)) <= 0) {
-            
-
-
 
             // Store the date and temperature in the map
             // ADD ANY OTHER MAP YOU PREFR HERE
