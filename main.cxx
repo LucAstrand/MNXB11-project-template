5c06fac412e2c72c4014a35432fe86e4231d0de1
Adib Shaker <neoadib@gmail.com>
2024-11-08 17:15:50 +0100

    update main.cxx

--- main.cxx
+++ main.cxx
@@ -6,19 +6,37 @@
 #include "date.h"
 #include "iso_week.h"
 #include <sstream>
+#include <lyra/lyra.hpp>
 
 using namespace date;
 using namespace std::chrono;
 
 int main(int argc, char *argv[]) {
-    // Check if a filename is provided
+    const char* filename = argv[1];
+    bool show_weekday = true;
+
+    // Set up Lyra CLI
+    auto cli = lyra::cli()
+        | lyra::opt(filename, "csv_file")
+              .name("file")
+              .help("")
+        | lyra::opt(show_weekday)
+              .name("-w")
+              .name("--no-weekday")
+              .help("");
+
+    // Commandline arguments
+    auto result = cli.parse({argc, argv});
+    if (!result) {
+        std::cerr << "Error: " << result.message() << std::endl;
+        return 1;
+    }
+
     if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " <csv_file>" << std::endl;
         return 1; // Return error code
     }
-
-    const char* filename = argv[1];
-
+    
     // Create a CSV reader object
     io::CSVReader<4> reader(filename); // Assuming there are 5 columns, but we'll skip the 4th
     reader.read_header(io::ignore_extra_column, "day", "year", "month", "measurement"); // Ignoring "ignoreme"
